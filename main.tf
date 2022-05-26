3a80c9026631496f6224dd385ee6ee824b8bc508
Oludare Odo <darekorex143@gmail.com>
2022-05-26 16:13:00 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -14,7 +14,7 @@ resource "aws_iam_role" "orchestration_role" {
       "StringEquals":{
         "token.actions.githubusercontent.com:sub": [
           "repo:ipipeline/${var.github_repo_name}:ref:refs/heads/test-devs",
-          "repo:ipipeline/${var.github_repo_name}:pull_requestss",
+          "repo:ipipeline/${var.github_repo_name}:pull_requests",
           "repo:ipipeline/${var.github_repo_name}:pull_requests"
         ]
       }
